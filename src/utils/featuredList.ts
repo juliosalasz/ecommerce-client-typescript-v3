5ceaf7f9bbdab28b31074ccdcd272bcb7bc8208b
Julio Salas <jscomic@gmail.com>
2022-09-28 01:24:27 -0500

    featured images updated

--- src/utils/featuredList.ts
+++ src/utils/featuredList.ts
@@ -1,22 +1,23 @@
+import { ServerUrl } from "../api/Api";
 export const FeaturedList = [
   {
     id: "123",
     name: "IPAD",
-    imageUrl: "https://i.ibb.co/Kmthg36/ipad.jpg",
+    imageUrl: `${ServerUrl}ipad.jpg`,
     price: "$200",
     route: "shop/videogames/playstation5",
   },
   {
     id: "456",
     name: "GEFORCE GTX 1080",
-    imageUrl: "https://i.ibb.co/GR6j0Lg/graphic-Card.jpg",
+    imageUrl: `${ServerUrl}graphic-Card.jpg`,
     price: "$300",
     route: "shop/VIDEOGAMES/951055",
   },
   {
     id: "789",
     name: "PLAYSTATION 5",
-    imageUrl: "https://i.ibb.co/ZfwVbPx/ps5.jpg",
+    imageUrl: `${ServerUrl}ps5.jpg`,
     price: "$500",
     route: "shop/videogames/playstation5",
     size: "large",
@@ -24,14 +25,14 @@ export const FeaturedList = [
   {
     id: "134",
     name: "SMARTWATCH XIAOMI",
-    imageUrl: "https://i.ibb.co/P1f7mtZ/smartwatch.jpg",
+    imageUrl: `${ServerUrl}smartwatch.jpg`,
     price: "$100",
     route: "shop/videogames/playstation5",
   },
   {
     id: "254",
     name: "HTC VR HEADSET",
-    imageUrl: "https://i.ibb.co/ftdG0Qj/vrheadset.jpg",
+    imageUrl: `${ServerUrl}vrheadset.jpg`,
     price: "$600",
     route: "shop/videogames/playstation5",
   },
